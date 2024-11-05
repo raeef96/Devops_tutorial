6bb1271f0e56a042fb779d15b29110794c44adfc
maxisr <34983449+Blezie@users.noreply.github.com>
2024-11-05 11:10:56 +0100

    update tutorial
    Co-Authored-By: Rafael Bechara <75490904+raeef96@users.noreply.github.com>

--- executable-tutorial/npm-security/step2/verify.sh
+++ executable-tutorial/npm-security/step2/verify.sh
@@ -3,7 +3,14 @@
 # Verify if the project directory was created and package.json exists
 if [ -f "npm-security/package.json" ]; then
   echo "Project setup verified successfully."
+  
+  # Check if node_modules directory exists
+  if [ -d "npm-security/node_modules" ]; then
+    echo "node_modules directory exists."
+  else
+    echo "node_modules directory is missing."
+  fi
 else
-  echo "Project setup failed. Please check your commands."
+  echo "Project setup failed."
   exit 1
 fi
--- executable-tutorial/npm-security/step4/verify.sh
+++ executable-tutorial/npm-security/step4/verify.sh
@@ -2,9 +2,11 @@
 
 # Verify if Snyk is installed
 if command -v snyk &> /dev/null; then
-  # Attempt to run snyk auth to check authentication
-  snyk auth --help &> /dev/null
-  if [ $? -ne 0 ]; then
+  # Attempt to run snyk test to check authentication
+  snyk test &> /dev/null
+  AUTH_STATUS=$?
+  
+  if [ $AUTH_STATUS -eq 401 ] || [ $AUTH_STATUS -ne 0 ]; then
     echo "Authentication failed. Please authenticate using 'snyk auth <YOUR_API_TOKEN>'"
     exit 1
   else
